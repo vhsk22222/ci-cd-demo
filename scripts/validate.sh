4572bc986e8dc18d311e9c8a126c8d8a055a435d
vhsk22222 <sravndeep5@gmail.com>
2026-06-27 17:22:14 +0530

    Update validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+echo "=================================="
+echo "Validating Website"
+echo "=================================="
+
+curl -f http://localhost
+
+if [ $? -eq 0 ]
+then
+    echo "Validation Successful"
+    exit 0
+else
+    echo "Validation Failed"
+    exit 1
+fi
