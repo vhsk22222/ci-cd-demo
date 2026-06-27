90699c624ae2cd5e6ab5770afb6579c4af343e33
vhsk22222 <sravndeep5@gmail.com>
2026-06-27 17:19:24 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo "=================================="
+echo "Cleanup Started"
+echo "=================================="
+
+rm -rf /var/www/html/*
+
+echo "Old application removed"
+
+echo "=================================="
+echo "Cleanup Completed"
+echo "=================================="
